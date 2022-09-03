6f6fecf02265f55c4c68cb27f34a3bbaf251a729
Roman Nizovtsev <romanizovtsev@mail.ru>
2022-09-04 01:21:38 +0300

    release

--- app/src/main/java/com/example/tasktest/ui/recycler/OnScrollListener.kt
+++ app/src/main/java/com/example/tasktest/ui/recycler/OnScrollListener.kt
@@ -1,25 +1,21 @@
 package com.example.tasktest.ui.recycler
 
-import android.graphics.Color.BLUE
-import android.util.Log
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.launch
 
-class OnScrollListener(val layoutManager: LinearLayoutManager, val adapter: RecyclerView.Adapter<RecyclerAdapter.ViewHolder>, var dataList: MutableList<DataModel>) : RecyclerView.OnScrollListener() {
-    var previousTotal = 0
-    var loading = true
-    val visibleThreshold = 10
-    var firstVisibleItem = 0
-    var visibleItemCount = 0
-    var totalItemCount = 0
-    var startNumber=3
+class OnScrollListener(
+    private val layoutManager: GridLayoutManager,
+    val callback: () -> Unit
+) : RecyclerView.OnScrollListener() {
+
+    private var previousTotal = 0
+    private var loading = true
+    private var firstVisibleItem = 0
+    private var visibleItemCount = 0
+    private var totalItemCount = 0
 
     override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
         super.onScrolled(recyclerView, dx, dy)
-
         visibleItemCount = recyclerView.childCount
         totalItemCount = layoutManager.itemCount
         firstVisibleItem = layoutManager.findFirstVisibleItemPosition()
@@ -31,36 +27,13 @@ class OnScrollListener(val layoutManager: LinearLayoutManager, val adapter: Recy
             }
         }
 
-        if (!loading && (totalItemCount - visibleItemCount) <= (firstVisibleItem + visibleThreshold)) {
-            val initialSize = dataList.size
-            Log.e("Загружаю","${startNumber}-${startNumber}")
-            updateDataList(dataList,startNumber+20)
-            val updatedSize = dataList.size
-            recyclerView.post { adapter.notifyItemRangeInserted(initialSize, updatedSize) }
+        if (!loading && (totalItemCount - visibleItemCount) <= (firstVisibleItem + VISIBLE_THRESHOLD)) {
+            callback()
             loading = true
         }
     }
-    private fun updateDataList(dataList: MutableList<DataModel>, endNumber: Int) {
-        var dataList2 =mutableListOf<DataModel>()
-            var i = startNumber
-            while (i < endNumber) {
-                if (isPrimeBruteForce(i)) {
-                    dataList2.add(DataModel(i, BLUE))
-                }
-                i += 2
-            }
-        this.dataList +=dataList2
-        startNumber=endNumber
-        }
 
-    private fun isPrimeBruteForce(number: Int): Boolean {
-        var i = 2
-        while (i*i <= number) {
-            if (number % i == 0) {
-                return false;
-            }
-            i++
-        }
-        return true;
+    private companion object {
+        private const val VISIBLE_THRESHOLD = 17
     }
 }
